e3729098fa91673f688430270e9133b3524ecdce
holmesdustin <holmes_dustin@columbusstate.edu>
2019-10-15 22:27:44 +0000

    test

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+var http = require('http');
+var path = require('path');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('views',Path2D.join(__dirname,'views'));
+app.set("view wngine",'ejs');
+
 app.get('/', function (req, res){
-    res.send("hello world");
+    res.render("index");
 });
 
 app.get('/about', function(req,res){
     res.send("<h1>about page</h1>");
 });
 
-app.listen(port, function(){
+http.createserver(app).listen(port, function(){
 
 });
